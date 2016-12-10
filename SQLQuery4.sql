ccddc8a7cd098b5a05f6e0cb42cafe12fc94aabd
Sun <ndnhuy2504@gmail.com>
2016-12-10 21:37:08 +0700

    delete file

--- SQLQuery4.sql
+++
@@ -1,6 +0,0 @@
-﻿select p.soPhong, lp.tenLoaiPhong, ks.tenKS, lp.donGia, ttp.tinhTrang from KhachSan ks, Phong p, LoaiPhong lp, TrangThaiPhong ttp
-where p.loaiPhong = lp.maLoaiPhong
-AND ks.maKS = lp.maKS
-AND p.maPhong = ttp.maPhong
-AND ttp.tinhTrang = N'Còn trống'
-
